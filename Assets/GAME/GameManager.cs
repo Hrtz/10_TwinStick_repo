9752bae8406390d59c702a7a8da247ef1a454690
Hrvoje <hrvoje.drinovac@gmail.com>
2017-11-29 17:41:18 +0100

    camera control added COURSE END STATE

--- Assets/GAME/GameManager.cs
+++ Assets/GAME/GameManager.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class GameManager : MonoBehaviour {
 
 	public bool recordOn = true;
 
+	private GameObject textPause;
+	private bool pauseOn;
+	private float fixedDelta;
+
 	//private ReplaySystem replaySystem;
 	
 	// Use this for initialization
 	void Start () {
+		pauseOn = false;
+		fixedDelta = Time.fixedDeltaTime;
+		textPause = GameObject.Find("TextPause");
+		textPause.SetActive(false);
 		//replaySystem = FindObjectOfType<ReplaySystem>();
 //		PlayerPrefsManager.UnlockLevel(1);
 //		print (PlayerPrefsManager.IsLevelUnlocked (0));
@@ -25,6 +34,14 @@ public class GameManager : MonoBehaviour {
 		} else {
 			recordOn = true;
 		}
+
+		if (!pauseOn && Input.GetKeyDown (KeyCode.P)) {
+			PauseGame ();
+		} else if (pauseOn && Input.GetKeyDown (KeyCode.P)) {
+			ResumeGame ();
+		}
+
+
 //		if (recordOn) {
 //			print ("RECORD OFF");
 //			replaySystem.PlayBack();
@@ -32,5 +49,27 @@ public class GameManager : MonoBehaviour {
 //			print ("RECORD ON");
 //			replaySystem.Record();
 //		}
+	}
+	void PauseGame () {
+		pauseOn = true;
+		Time.timeScale = 0;
+		Time.fixedDeltaTime = 0;
+		print ("pause");
+		textPause.SetActive(true);
+	}
+
+	void ResumeGame () {
+		pauseOn = false;
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = fixedDelta;
+		textPause.SetActive(false);
+	}
+
+	void OnApplicationPause (bool pauseStatus)
+	{	
+		pauseOn = pauseStatus;
+		if (pauseOn) {
+			PauseGame ();
+		}
 	}
 }
